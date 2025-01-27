5009b58b708032a811bdbe90cb98700769d8e783
jdrguez <josedomingo.rguez.rguez@gmail.com>
2025-01-27 19:33:35 +0000

    update games views <3

--- games/views.py
+++ games/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import get_list_or_404, get_object_or_404
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
 from .models import Game, Review
 from .serializer import GameSerializer
 
+
 def game_list(request):
-    games = Game.objects.all()
+    if category := request.GET.get('category'):
+        games = Game.objects.filter(category__slug=category)
+    if platform := request.GET.get('platform'):
+        games = Game.objects.filter(platforms__slug=platform)
+    if len(request.GET.keys()) == 2:
+        category = request.GET.get('category')
+        platform = request.GET.get('platform')
+        games = Game.objects.filter(platforms__slug=platform, category__slug=category)
+    else:
+        games = Game.objects.all()
+
     games_json = GameSerializer(games, request=request)
     return games_json.json_response()
 
 
 def game_detail(request, game_slug):
-    game = get_object_or_404(Game, slug=game_slug)
-    pass
+    game = Game.objects.get(slug=game_slug)
+    game_json = GameSerializer(game, request=request)
+    return game_json.json_response()
 
 
 def review_list(request, game_slug):
